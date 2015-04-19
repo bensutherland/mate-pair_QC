5fdc5cda165a35f3b8c3a456474843dff98324f5
bensutherland <bensuth@katak.ibis.ulaval.ca>
2015-04-19 11:44:10 -0400

    fixed binning script

--- 01_scripts/02_NxTrim_binning.sh
+++ 01_scripts/02_NxTrim_binning.sh
@@ -6,11 +6,9 @@ TRIMMED_FOLDER="03_trimmed_data"
 BINNED_FOLDER="04_binned_mps"
 NXTRIM_PROGRAM="/project/lbernatchez/drobo/users/bensuth/programs/NxTrim/nxtrim"
 
-
 # binning mate-pair MI.Seq data within the trimmed folder
 ls -1 $TRIMMED_FOLDER/*.fastq.gz | \
     perl -pe 's/R[12]\.paired\.fastq\.gz//' | \
-    grep -vE "paired|single" | \
     sort -u | \
     while read i
     do
@@ -18,6 +16,6 @@ ls -1 $TRIMMED_FOLDER/*.fastq.gz | \
 	$NXTRIM_PROGRAM \
 		-1 "$i"R1.paired.fastq.gz \
 		-2 "$i"R2.paired.fastq.gz \
-		-O $BINNED_FOLDER/"$i" \
+		-O $BINNED_FOLDER/ \
 		--justmp
     done
