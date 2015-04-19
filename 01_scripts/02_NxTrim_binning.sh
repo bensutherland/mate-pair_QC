c9d4c88b7501c428d2088b67d423d2408f73e529
bensutherland <bensuth@katak.ibis.ulaval.ca>
2015-04-19 11:30:11 -0400

    improving binning script

--- 01_scripts/02_NxTrim_binning.sh
+++ 01_scripts/02_NxTrim_binning.sh
@@ -4,20 +4,20 @@
 # Global variables
 TRIMMED_FOLDER="03_trimmed_data"
 BINNED_FOLDER="04_binned_mps"
-NXTRIM_PROGRAM="/Users/wayne/Documents/bioinformatics/NxTrim/nxtrim"
+NXTRIM_PROGRAM="/project/lbernatchez/drobo/users/bensuth/programs/NxTrim/nxtrim"
 
 
 # binning mate-pair MI.Seq data within the trimmed folder
 ls -1 $TRIMMED_FOLDER/*.fastq.gz | \
-    perl -pe 's/R[12]\.fastq\.gz//' | \
+    perl -pe 's/R[12]\.paired\.fastq\.gz//' | \
     grep -vE "paired|single" | \
     sort -u | \
     while read i
     do
        echo $i
 	$NXTRIM_PROGRAM \
-		-1 "$i"R1.fastq.gz \
-		-2 "$i"R2.fastq.gz \
+		-1 "$i"R1.paired.fastq.gz \
+		-2 "$i"R2.paired.fastq.gz \
 		-O $BINNED_FOLDER/"$i" \
 		--justmp
     done
