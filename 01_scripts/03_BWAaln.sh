8a3e203c445402521e3f2c9ee3979a0661416d09
bensutherland <bensutherland7@gmail.com>
2015-04-23 19:00:54 -0400

    removed indexing from alignment

--- 01_scripts/03_BWAaln.sh
+++ 01_scripts/03_BWAaln.sh
@@ -11,14 +11,8 @@ RG[1]='@RG\tID:lib208\tSM:lib208\tPL:Illumina'
 RG[2]='@RG\tID:lib209\tSM:lib209\tPL:Illumina'
 RG[3]='@RG\tID:lib210\tSM:lib210\tPL:Illumina'
 
-# unzip fasta
-# gunzip -c /project/lbernatchez/drobo/users/bensuth/00_resources/Ssa_ASM_3.6.fasta.gz > /project/lbernatchez/drobo/users/bensuth/00_resources/Ssa_ASM_3.6.fasta
-
 #Create a shell variable to store the location of our reference transcriptome
 REFERENCE=/project/lbernatchez/drobo/users/bensuth/00_resources/Ssa_ASM_3.6.fasta
-
-#Index reference transcriptome
-# bwa index $REFERENCE
 
 #Map the reads
 for i in 1 2 3
