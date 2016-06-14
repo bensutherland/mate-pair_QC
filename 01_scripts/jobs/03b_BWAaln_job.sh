5dfe9ec7efe18426b4f8d2cc9b615427fce9ec5f
bensutherland <bensutherland7@gmail.com>
2016-06-14 11:02:50 -0400

    Change job file to SLURM

--- 01_scripts/jobs/03b_BWAaln_job.sh
+++ 01_scripts/jobs/03b_BWAaln_job.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-#$ -N bwa
-#$ -M bensutherland7@gmail.com 
-#$ -m beas
-#$ -pe smp 10
-#$ -l h_vmem=100G
-#$ -l h_rt=14:00:00
-#$ -cwd
-#$ -S /bin/bash
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=bensutherland7@gmail.com
+#SBATCH -J bwa
+#SBATCH --cpus-per-task=10
+#SBATCH --time=60:00:00
+#SBATCH --mem=40000
 
 time ./01_scripts/03b_BWAaln.sh
 
